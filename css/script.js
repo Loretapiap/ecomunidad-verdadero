a8e3c213dedb6deb3a746820dc7fdd575ce2e93b
Loretapiap <loretapiap@gmail.com>
2016-04-04 17:21:44 -0300

    Grupal

---
+++ css/script.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+var i = 0;
+$(document).on("ready", main);
+
+function main(){
+var control = setInterval(cambiarSlider, 3000);
+}
+
+function cambiarSlider(){
+i++;
+if(i == $("#slider img").size()){
+i = 0;
+}
+$("#slider img").hide();
+$("#slider img").eq(i).fadeIn("medium");
+}
+</script>
